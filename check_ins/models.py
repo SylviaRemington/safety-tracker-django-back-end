88ab2ea179a34fda358802b1769ba90eec36dd2b
SylviaRemington <sylviaremington@gmail.com>
2025-10-31 15:57:47 -0700

    addtl max length changes to checkins model

--- check_ins/models.py
+++ check_ins/models.py
@@ -5,13 +5,13 @@ class Check_In(models.Model):
     def __str__(self):
         return f'{self.title} - {self.category}'
     
-    title = models.CharField(max_length=1000, unique=True)
-    description = models.CharField(max_length=100000)
-    day_type = models.CharField(max_length=100, default="Not specified")  
-    relaxed_today = models.CharField(max_length=100, default="Not specified") 
-    category = models.CharField(max_length=80)
+    title = models.CharField(max_length=10000, unique=True)
+    description = models.CharField(max_length=1000000)
+    day_type = models.CharField(max_length=1000, default="Not specified")  
+    relaxed_today = models.CharField(max_length=1000, default="Not specified") 
+    category = models.CharField(max_length=1000)
     reaction_level = models.FloatField()
-    coping_action = models.CharField(max_length=5000)
+    coping_action = models.CharField(max_length=10000)
     effectiveness = models.FloatField()
     owner = models.ForeignKey(
         "authentication.User",
